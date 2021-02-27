62161d2bb9ad006f96b7ddd07bb0544fefa2ddcf
nic <nicoladochnenko2@gmail.com>
2021-02-27 20:00:34 +0000

    trying to fix file usage

--- src/App.js
+++ src/App.js
@@ -1,35 +1,51 @@
 import './App.css';
+import fs from 'fs';
 import React, { useState } from 'react';
 import Waveform from './components/Waveform';
 import { Button, ButtonLabel } from './components/AudioPlayer/audioplayer';
 import WrapperMain from './components/Wrapper/wrapper';
 
+// TODO: fs (file management)
+// User upload or pre-uploaded
+// Move files into same folder
+
 function App() {
   const [file, setFile] = useState();
+  const [fileName, setFileName] = useState();
   const [isSelected, setIsSelected] = useState(false);
 
   const changeHandler = (event) => {
-      setFile(event.target.files[0]);
+    const reader = new FileReader();
+      setFile(document.querySelector('input[type=file]').files[0]);
+      reader.addEventListener("load", function () {
+        setFile(reader.result);
+      }, false);
+
+      if (file) {
+        reader.readAsDataURL(file);
+      }
+      console.log(file, 'file');
       setIsSelected(true);
   };
+
+  console.log(file, 'filereader file');
+
   const handleSubmission = () => {
 
   };
 
-  console.log(file);
-
   return (
     <div className="App">
       <header className="App-header">
-        <h1>The Colour of Music</h1>
-        <Waveform track={file}/>
+        <h1>The Colour of Music</h1>  
         This app aims to replicate the
         experience of music through the
-        use of colour.
+        use of colour. 
+        <Waveform track={file}/>
         <WrapperMain>
-        <Button type="file" id="user-file-upload" accept=".mp3" onChange={changeHandler} onClick={handleSubmission} hidden/>
+        <Button type="file" id="user-file-upload" onChange={changeHandler} onClick={handleSubmission} hidden/>
         <ButtonLabel htmlFor="user-file-upload"> Upload audio file</ButtonLabel>
-        {isSelected ? ( <p>{file.name}</p>
+        {isSelected && file ? ( <p>{file.name}</p>
          ) : (
              <p>No File Selected</p>
          )}
--- src/components/Waveform/index.js
+++ src/components/Waveform/index.js
@@ -1,111 +1,61 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import WaveSurfer from 'wavesurfer.js';
+
 import { WaveformContainer, Wave, PlayButton } from './waveformStyle';
 
 function Waveform(track) {
 
-  var wavesurferBlank = WaveSurfer.create({
-    container: '#waveform',
-  });
+  const ref = useRef();
 
-  const [waveform, setWaveform] = useState();
-  var wavesurfer1 = WaveSurfer.create({
-    container: '#waveform',
-    barWidth: 3,
-    cursorWidth: 1,
-    backend: 'WebAudio',
-    height: 200,
-    barGap: 3,
-    barRadius: 3,
-    progressColor: '#2D5BFF',
-    responsive: true,
-    waveColor: '#EFEFEF',
-    cursorColor: 'transparent',
-  });
-  var wavesurfer2 = WaveSurfer.create({
-    container: '#waveform',
-    barWidth: 3,
-    cursorWidth: 1,
-    backend: 'WebAudio',
-    height: 200,
-    barGap: 3,
-    barRadius: 3,
-    progressColor: '#2D5BFF',
-    responsive: true,
-    waveColor: '#EFEFEF',
-    cursorColor: 'transparent',
-  });
+  // var wavesurferBlank = WaveSurfer.create({
+  //   container: '#waveform',
+  // });
 
+  console.log(track);
 
   useEffect(() => {
-    if (track !== undefined){
-      if (track.name.includes('major')) {
-        setWaveform(wavesurfer1);
-        wavesurfer1.load(track);
-        wavesurfer1.drawBuffer(); 
-      }
-       else if (track.name.includes('minor')) { 
-      console.log('progress');
-      setWaveform(wavesurfer2);
-      wavesurfer2.load(track);
-      wavesurfer2.drawBuffer(); 
-       }
-    } else {
-      setWaveform(wavesurferBlank); 
-       }
+    if (track && ref.current){
+      var wavesurfer = WaveSurfer.create({
+        container: ref.current,
+        // barWidth: 30,
+        // cursorWidth: 1,
+        backend: 'WebAudio',
+        // mediaControls: true,
+        height: 200, 
+        // barGap: 3,
+        // barRadius: 3,
+        progressColor: '#2D5BFF',
+        responsive: true,
+        waveColor: '#EFEFEF'
+        // cursorColor: 'transparent',
+      });
+
+      // wavesurfer.on('ready', function(){
+      //   console.log('i am here');
+      //   wavesurfer.play();
+      // });
+      wavesurfer.load(track);
+      console.log(wavesurfer);
+      // wavesurfer.drawer
+      wavesurfer.drawBuffer();
+    }
+    return () => {
+      wavesurfer.destroy();
+    }
   }, [track]);
+
+  // handlePlay = () => {
+  //   this.setState({ playing: !this.state.playing });
+  //   this.wavesurfer.playPause();
+  // };
+
     return (
-      <div id="waveform">{waveform}</div>
+      <WaveformContainer>
+      <Wave ref={ref}></Wave>
+      <audio controls id="track" src={track} type="audio/mpeg"/> {console.log(track, 'track!!!!!!')}
+      {/* <PlayButton onClick={this.}>test</PlayButton> */}
+      </WaveformContainer>
     );
 };
-//       <WaveformContainer>
-//         <PlayButton>
-//           test
-//         </PlayButton>
-//         <Wave id="waveform" />
-//       </WaveformContainer>
-//     );
-// };
-
-// class Waveform extends Component {  
-
-//   componentDidMount() {
-//     const track = this.props.dataFromParent;
-
-//     this.waveform = WaveSurfer.create({
-//       barWidth: 3,
-//       cursorWidth: 1,
-//       container: '#waveform',
-//       backend: 'WebAudio',
-//       height: 80,
-//       progressColor: '#2D5BFF',
-//       responsive: true,
-//       waveColor: '#EFEFEF',
-//       cursorColor: 'transparent',
-//     });
-
-//     this.waveform.load(track);
-//   };
-
-//   handlePlay = () => {
-//     this.setState({ playing: !this.state.playing });
-//     this.waveform.playPause();
-//   };
-  
-//   render(track) {
-//     if (track !== null) {
-//       return (
-//         <WaveformContainer>
-//           <PlayButton>
-//             test
-//         </PlayButton>
-//           <Wave id="waveform" />
-//           <audio id="track"/>
-//         </WaveformContainer>
-//       )
-//     } else {
-//       return (<WaveformContainer>Loading...</WaveformContainer>)
-//     }}
-// };
 
 export default Waveform;
